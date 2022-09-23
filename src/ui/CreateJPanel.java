724b03b0074b9b582bd1061bd3d5a9be2ccd9fc2
Rui SHANG <113535135+sanggiya-neu@users.noreply.github.com>
2022-09-23 18:17:57 +0800

    Minor updates

--- src/ui/CreateJPanel.java
+++ src/ui/CreateJPanel.java
@@ -106,7 +106,7 @@ public class CreateJPanel extends javax.swing.JPanel {
 
         careerLabel.setText("Career");
 
-        teleLabel.setText("Telephone Numer");
+        teleLabel.setText("Telephone Number");
         teleLabel.setToolTipText("");
 
         degreeLabel1.setText("Degree 1");
